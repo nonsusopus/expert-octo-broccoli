115ef6b4fdfcb1a3d578a736a7fc1ff10fa751b5
Anon Opus <anonopus@proton.me>
2024-05-08 08:18:30 +1000

    Rename file from script.js.save to applied-math-functions.js

---
+++ applied-math-functions.js
@@ -0,0 +1,14 @@
+// apply-math-operation.js
+
+// Import the functions from basic-math-functions.js
+const{add,subtract,multiply,divide}=require('./basic-math-operations');
+
+// Define some numbers 
+const num1=10;
+const num2=5;
+
+// Apply the funtions 
+console.log('Addition:$(add(num1,num2)}');
+console.log('Subtraction:$(subtract(num1,num2)}');
+console.log('Multiplication:$(multiplication(num1,num2)}');
+console.log('Division:$(division(num1,num2)}');
