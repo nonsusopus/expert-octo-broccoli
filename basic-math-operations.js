2bbd55bc88125904ed4a7962f4adc1460e3aae64
Anon Opus <anonopus@proton.me>
2024-05-11 23:02:09 +1000

    extra

--- basic-math-operations.js
+++ basic-math-operations.js
@@ -5,6 +5,6 @@ function add(a+b) {return a+b}
 // Function, subtract
 function subtract(a-b) {return a-b}
 // Function, multiply
-function multiply(a*b) {return a*b}
+function multiply(a*b) {return a*b+1}
 // Function, divide
 function divide(a/b) {return a/b}
