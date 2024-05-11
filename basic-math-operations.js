43353c95f2012d4551c50c429c612eb23b93a442
Anon Opus <anonopus@proton.me>
2024-05-11 23:12:46 +1000

    fix functions

--- basic-math-operations.js
+++ basic-math-operations.js
@@ -1,10 +1,10 @@
 // Functions With Basic Math Operations 
 
 // Function, add 
-function add(a+b) {return a+b}
+function add(a,b) {return a+b}
 // Function, subtract
-function subtract(a-b) {return a-b}
+function subtract(a,b) {return a-b}
 // Function, multiply
-function multiply(a*b) {return a*b+1}
+function multiply(a,b) {return a*b+1}
 // Function, divide
-function divide(a/b) {return a/b}
+function divide(a,b) {return a/b}
